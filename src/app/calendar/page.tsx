1bab32587405bc57444291155f7ef49f08af7fb1
teegoood <teegooodgood@gmail.com>
2024-04-18 20:43:11 +0700

    add: navbar link

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { Button } from "@mui/material";
 import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Image from "next/image";
+import Link from "next/link";
 
 const mockData = [
   {
@@ -42,24 +44,25 @@ const mockData = [
 
 const Calendar = () => {
   return (
-    <div className="h-full max-w-5xl mx-auto mt-8">
+    <div className="h-full max-w-[800px] mx-auto my-8">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar className="bg-stone-200 rounded-lg mb-8" />
+        <DateCalendar className="bg-stone-200 rounded-lg mb-8 w-full" />
       </LocalizationProvider>
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center gap-4 w-full">
         {mockData.map((data) => (
-          <div>
+          <div className="w-full">
             <div className="mb-2 font-bold">{data.date}</div>
-            <div className="flex rounded-lg overflow-hidden bg-white gap-2 shadow-lg sha">
+            <Link href={"/events/:id"} className="flex rounded-lg overflow-hidden bg-white gap-2 shadow-lg">
               <Image
                 src={data.imageUrl}
-                width={150}
-                height={150}
+                width={200}
+                height={200}
                 alt="event-image"
+                className="w-fit"
               />
-              <div className="p-2">
-                <div>{data.date}</div>
-                <div>{data.name}</div>
+              <div className="p-4 flex flex-col gap-2">
+                <div className="font-bold text-red-400">{data.date}</div>
+                <div className="text-lg font-bold">{data.name}</div>
                 <div>{data.location}</div>
                 <div className="flex gap-2 items-center">
                   <Icon icon="fluent:people-community-48-filled" width={30} />
@@ -68,11 +71,16 @@ const Calendar = () => {
                 <div className="flex gap-2">
                   #tag
                   {data.tags.map((tag) => (
-                    <div className="bg-green-200 px-4 rounded-full text-xs font-bold flex items-center">{tag}</div>
+                    <div className="bg-green-200 px-4 rounded-full text-xs font-bold flex items-center">
+                      {tag}
+                    </div>
                   ))}
                 </div>
+                <Button className="bg-red-400 text-white font-bold text-md rounded-lg w-fit px-10 mt-4">
+                  view detail
+                </Button>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,22 @@
-"use client"
+"use client";
 
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between px-20 py-4 items-center bg-white shadow-lg">
-      <span className="text-4xl font-bold text-pink-400">CUact</span>
+      <Link className="text-4xl font-bold text-pink-400" href={"/"}>
+        CUact
+      </Link>
       <div className="flex gap-8">
-        <Icon icon="solar:calendar-bold" width={30}/>
-        <Icon icon="mdi:bell-ring" width={30}/>
-        <Icon icon="iconamoon:profile-circle-fill" width={30}/>
+        <Link href={"/calendar"}>
+          <Icon icon="solar:calendar-bold" width={30} />
+        </Link>
+        <Link href={"/notification"}>
+          <Icon icon="mdi:bell-ring" width={30} />
+        </Link>
+        <Icon icon="iconamoon:profile-circle-fill" width={30} />
       </div>
     </nav>
   );
