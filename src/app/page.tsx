84de8284e7aaf55a6b0c7bc880ea214aff3a3e9b
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2024-04-19 01:39:20 +0700

    add login system and adjust files
    adjust nav bar by add login menu and search bar also relocate some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-full h-screen">
-      test na ja eiei
-    </div>
-  );
+  return <div className="w-full h-screen">test na ja eiei</div>;
 }
