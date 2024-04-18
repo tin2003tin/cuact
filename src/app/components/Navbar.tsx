035a02801d5c9f07e0dd15f2656def8956a35ab7
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2024-04-18 20:23:34 +0700

    feat: add Link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,22 @@
 "use client"
 
 import { Icon } from "@iconify/react";
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between px-20 py-4 items-center bg-white">
       <span className="text-4xl font-bold text-pink-400">CUact</span>
-      <div className="flex gap-8">
-        <Icon icon="solar:calendar-bold" width={30}/>
-        <Icon icon="mdi:bell-ring" width={30}/>
-        <Icon icon="iconamoon:profile-circle-fill" width={30}/>
+      <div className="flex gap-8 text-black">
+        <Link href="/calendar">
+          <Icon icon="solar:calendar-bold" width={30}/>
+        </Link>
+        <Link href="/notifications">
+          <Icon icon="mdi:bell-ring" width={30}/>
+        </Link>
+        <Link href="/profile">
+          <Icon icon="iconamoon:profile-circle-fill" width={30}/>
+        </Link>
       </div>
     </nav>
   );
