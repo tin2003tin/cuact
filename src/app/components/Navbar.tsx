54331af61dc1c2b5aa933a9ffbd7da977dd91a5a
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2024-04-18 23:23:47 +0700

    yeah

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Icon } from "@iconify/react";
 
@@ -7,9 +7,9 @@ const Navbar = () => {
     <nav className="flex justify-between px-20 py-4 items-center bg-white">
       <span className="text-4xl font-bold text-pink-400">CUact</span>
       <div className="flex gap-8">
-        <Icon icon="solar:calendar-bold" width={30}/>
-        <Icon icon="mdi:bell-ring" width={30}/>
-        <Icon icon="iconamoon:profile-circle-fill" width={30}/>
+        <Icon icon="solar:calendar-bold" width={30} />
+        <Icon icon="mdi:bell-ring" width={30} />
+        <Icon icon="iconamoon:profile-circle-fill" width={30} />
       </div>
     </nav>
   );
