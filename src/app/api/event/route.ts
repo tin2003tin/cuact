f831667e51c20ac134e5ff82be05a9093a0c710c
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2024-04-21 03:59:25 +0700

    add search

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -1,15 +1,54 @@
 import { PrismaClient } from "@prisma/client";
+import { useSearchParams } from "react-router-dom";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const events = await prisma.event.findMany({
-    include: {
-      Category: true,
-    },
-  });
-  return NextResponse.json(events);
+export async function GET(request: NextRequest) {
+  let search = request.nextUrl.searchParams.get("search");
+  const tags = request.nextUrl.searchParams.getAll("tag");
+  if (!search) {
+    search = "";
+  }
+  try {
+    const events = await prisma.event.findMany({
+      where: {
+        name: {
+          contains: search,
+          mode: "insensitive",
+        },
+      },
+      include: {
+        Category: true,
+      },
+    });
+
+    let filteredEvents = [];
+    if (tags.length > 0) {
+      for (let i = 0; i < events.length; i++) {
+        let valid = true;
+        let categories = [];
+        for (let j = 0; j < events[i].Category.length; j++) {
+          categories.push(events[i].Category[j].name);
+        }
+        for (var k = 0; k < tags.length; k++) {
+          if (!categories.includes(tags[k])) {
+            valid = false;
+          }
+        }
+        if (valid) {
+          filteredEvents.push(events[i]);
+        }
+      }
+    } else {
+      filteredEvents = events;
+    }
+
+    return NextResponse.json(filteredEvents);
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function POST(request: Request) {
