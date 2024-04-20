74bbdb8de20dd5fce9091e90fc82b549e8102eff
teegoood <teegooodgood@gmail.com>
2024-04-20 20:40:16 +0700

    add: isEdit mode nav bar

--- src/components/profile/ProfilePopUp.tsx
+++ src/components/profile/ProfilePopUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Unstable_Popup as BasePopup } from "@mui/base/Unstable_Popup";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import {
@@ -19,7 +19,6 @@ const ProfilePopUp = () => {
 
   const open = Boolean(anchor);
   const id = open ? "simple-popper" : undefined;
-  const { isLoading, user } = useKindeBrowserClient();
 
   return (
     <div>
@@ -27,59 +26,82 @@ const ProfilePopUp = () => {
         <Icon icon="gg:profile" width={30} />
       </div>
       <BasePopup id={id} open={open} anchor={anchor}>
-        <div className="bg-white p-6 rounded-lg shadow-lg max-w-[350px] ">
-          <div className="border rounded-lg p-4 mb-4">
-            <div className="flex justify-between items-center border-b-2 mb-4">
-              <span># My Tag</span>
-              <Icon icon="mdi:pencil" />
-            </div>
-            <div className="flex flex-wrap gap-2 ">
-              {tags.map((tag) => (
-                <Tag text={tag} />
-              ))}
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            {user && <div className="flex items-center gap-2">
-              <Icon icon="iconamoon:profile-fill" />
-              Profile setting
-            </div>}
-            {user &&<div className="flex items-center gap-2">
-              <Icon icon="octicon:arrow-switch-16" />
-              Switch to creator
-            </div>}
-            {!user && (
-              <>
-                <LoginLink postLoginRedirectURL="" className="list flex gap-2 item-center">
-                  <Icon icon={"uil:signin"}></Icon>Sign In
-                </LoginLink>
-              </>
-            )}
-            {!user && (
-              <>
-                <RegisterLink postLoginRedirectURL="/" className="list flex gap-2 item-center">
-                  <Icon icon={"mdi:register"}></Icon>Sign Up
-                </RegisterLink>
-              </>
-            )}
-            {user && (
-              <>
-                <LogoutLink className="list flex gap-2 item-center">
-                  <Icon icon={"uil:signin"}></Icon>Log Out
-                </LogoutLink>
-              </>
-            )}
-          </div>
-        </div>
+        <PopUp />
       </BasePopup>
     </div>
   );
 };
 
-const Tag = ({ text }: { text: string }) => {
+const PopUp = () => {
+  const { user } = useKindeBrowserClient();
+  const [isEdit, setIsedit] = useState(false);
+
   return (
-    <div className="bg-green-400 px-4 rounded-full text-sm flex items-center text-white">
+    <div className="bg-white p-6 rounded-lg shadow-lg max-w-[400px] ">
+      <div className="border rounded-lg p-4 mb-4">
+        <div
+          className="flex justify-between items-center border-b-2 mb-4"
+          onClick={() => setIsedit((prev) => !prev)}
+        >
+          <span># My Tag</span>
+          <Icon icon={`${isEdit ? "fluent-mdl2:check-mark" : "mdi:pencil"}`} />
+        </div>
+        <div className="flex flex-wrap gap-2 ">
+          {tags.map((tag) => (
+            <Tag text={tag} isEdit={isEdit} />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        {user && (
+          <div className="flex items-center gap-2">
+            <Icon icon="iconamoon:profile-fill" />
+            Profile setting
+          </div>
+        )}
+        {user && (
+          <div className="flex items-center gap-2">
+            <Icon icon="octicon:arrow-switch-16" />
+            Switch to creator
+          </div>
+        )}
+        {!user && (
+          <>
+            <LoginLink
+              postLoginRedirectURL=""
+              className="list flex gap-2 item-center"
+            >
+              <Icon icon={"uil:signin"}></Icon>Sign In
+            </LoginLink>
+          </>
+        )}
+        {!user && (
+          <>
+            <RegisterLink
+              postLoginRedirectURL="/"
+              className="list flex gap-2 item-center"
+            >
+              <Icon icon={"mdi:register"}></Icon>Sign Up
+            </RegisterLink>
+          </>
+        )}
+        {user && (
+          <>
+            <LogoutLink className="list flex gap-2 item-center">
+              <Icon icon={"uil:signin"}></Icon>Log Out
+            </LogoutLink>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Tag = ({ text, isEdit }: { text: string; isEdit: Boolean }) => {
+  return (
+    <div className="bg-green-400 px-4 rounded-full text-sm flex gap-2 items-center text-white">
       {text}
+      {isEdit && <Icon icon="material-symbols:close" className="text-lg" />}
     </div>
   );
 };
