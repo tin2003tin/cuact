f146ab3dc623bb80749f55984438e2dd76cc0957
teegoood <teegooodgood@gmail.com>
2024-04-20 15:19:16 +0700

    add: bigcard

---
+++ src/app/components/BigCard.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Button } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const mockData = [
+  {
+    date: "fri. 12 Apr 2024",
+    name: "Hack Chula",
+    location: "ตึก... คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย",
+    joined: 6969,
+    imageUrl: "/hackChula.jpg",
+    tags: ["Tech", "Competition", "Hackathon"],
+  },
+  {
+    date: "fri. 12 Apr 2024",
+    name: "Hack Chula",
+    location: "ตึก... คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย",
+    joined: 6969,
+    imageUrl: "/hackChula.jpg",
+    tags: ["Tech", "Competition", "Hackathon"],
+  },
+  {
+    date: "fri. 12 Apr 2024",
+    name: "Hack Chula",
+    location: "ตึก... คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย",
+    joined: 6969,
+    imageUrl: "/hackChula.jpg",
+    tags: ["Tech", "Competition", "Hackathon"],
+  },
+  {
+    date: "fri. 12 Apr 2024",
+    name: "Hack Chula",
+    location: "ตึก... คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย",
+    joined: 6969,
+    imageUrl: "/hackChula.jpg",
+    tags: ["Tech", "Competition", "Hackathon"],
+  },
+];
+
+const BigCard = ({ data }: {data}) => {
+  return (
+    <div>
+      {mockData.map((data) => (
+        <div className="w-full">
+          <div className="mb-2 font-bold text-xl">{data.date}</div>
+          {/* card */}
+          <div className="flex rounded-2xl overflow-hidden bg-white gap-2 shadow-lg hover:shadow-xl">
+            <Image
+              src={data.imageUrl}
+              width={200}
+              height={200}
+              alt="event-image"
+              className="w-fit"
+            />
+            <div className="p-8 flex flex-col gap-2 text-white bg-black bg-opacity-90 w-full">
+              <div className="font-bold text-red-400">{data.date}</div>
+              <div className="text-xl font-bold">{data.name}</div>
+              <div className="text-sm">{data.location}</div>
+              <div className="flex gap-2 items-center text-sm">
+                <Icon icon="fluent:people-community-48-filled" width={30} />
+                {data.joined} คน
+              </div>
+              <div className="flex gap-2">
+                #tag
+                {data.tags.map((tag) => (
+                  <div className="bg-green-400 px-4 rounded-full text-xs font-bold flex items-center">
+                    {tag}
+                  </div>
+                ))}
+              </div>
+              <Link href={"/events/:id"}>
+                <Button
+                  className="bg-red-400 text-white font-bold text-md rounded-lg w-fit px-10 mt-4 hover:bg-red-200"
+                  variant="contained"
+                >
+                  view detail
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default BigCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import BigCard from "./components/BigCard";
+
 export default function Home() {
-  return <div className="w-full h-screen">test na ja eiei</div>;
+  return (
+    <div className="w-full h-screen">
+      test na ja eiei
+      <BigCard />
+    </div>
+  );
 }
